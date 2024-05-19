f8ee1d00109c7cbd194480c5371de2819d99368d
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2024-05-19 13:41:09 +0300

    rename from "Fastor" to "App" to make it allow to copy/past to any project without use plugin fastor

--- test/fastor_app_ui_widget_test.dart
+++ test/fastor_app_ui_widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fastor_app_ui_widget/fastor_app_ui_widget.dart';
+// import 'package:fastor_app_ui_widget/fastor_app_ui_widget.dart';
 
 void main() {
   // test('adds one to input values', () {
