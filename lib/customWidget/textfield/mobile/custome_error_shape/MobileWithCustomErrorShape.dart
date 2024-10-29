62a19722cc76cd46e2eb012672974d5834f54082
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2024-10-29 11:05:23 +0300

    MobileWithCustomErrorShape

---
+++ lib/customWidget/textfield/mobile/custome_error_shape/MobileWithCustomErrorShape.dart
@@ -0,0 +1,219 @@
+import 'package:fastor_app_ui_widget/core/boarder/BoarderHelper.dart';
+import 'package:fastor_app_ui_widget/core/device/DeviceTools.dart';
+import 'package:fastor_app_ui_widget/core/figma/Figma.dart';
+import 'package:fastor_app_ui_widget/core/lang/PositionedApp.dart';
+import 'package:fastor_app_ui_widget/core/log/Log.dart';
+import 'package:fastor_app_ui_widget/core/resource/ColorResource.dart';
+import 'package:fastor_app_ui_widget/core/resource/DimensionResource.dart';
+import 'package:fastor_app_ui_widget/core/values/ToolsValidation.dart';
+import 'package:fastor_app_ui_widget/customWidget/text/TextApp.dart';
+import 'package:fastor_app_ui_widget/customWidget/textfield/mobile/country_only/CountryTextFieldPicker.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:fastor_app_ui_widget/customWidget/textfield/mobile/MobileCountryApp.dart';
+import 'package:fastor_app_ui_widget/customWidget/textfield/regular/TextFieldApp.dart';
+
+import 'package:fastor_app_ui_widget/core/lang/LangApp.dart';
+
+class MobileWithCustomErrorShape extends StatelessWidget {
+
+
+  final marginBetweenTwoField = 10.0;
+
+  String labelText;
+  OutlineInputBorder? outlineInputBoarder;
+  TextEditingController? controller;
+  FormFieldValidator<String>? validatorCustom;
+  MobileCountryListener callback;
+  String? errorMessage;
+  String? errorKeySearchingInErrorMessageArray;
+  Map<String, dynamic>? errorsMessageArray;
+
+  // Color? colorLineBoarder;
+
+  // bool _hasFocus = false;
+
+  String? phone_text;
+  String? countryCode  ;
+  BuildContext? context;
+
+
+  MobileWithCustomErrorShape({
+    required this.labelText,
+    required this.callback,
+    this.controller,
+    this.validatorCustom,
+    this.errorsMessageArray,
+    this.errorKeySearchingInErrorMessageArray,
+    this.errorMessage,
+    this.outlineInputBoarder,
+}){
+
+    // colorLineBoarder ??= ColorResource.textFieldDarkBoarderLineBeforeFocused;
+    _setValidatorFromBackend();
+  }
+
+
+  void _setValidatorFromBackend() {
+    if (errorKeySearchingInErrorMessageArray == null) return;
+    if (errorsMessageArray == null) return;
+    if (errorsMessageArray!.containsKey(errorKeySearchingInErrorMessageArray!) == false) return;
+    errorMessage = errorsMessageArray!["" + errorKeySearchingInErrorMessageArray!][0];
+    Log.i( "MobileWithCustomErrorShape - _setValidatorFromBackend() - errorMessage: $errorMessage");
+  }
+
+
+
+
+
+  //--------------------------------------------------------------  content ui
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    return   Directionality(
+      textDirection:   TextDirection.ltr,
+      child:   Builder(
+        builder: (BuildContext context) {
+          return   MediaQuery(
+            data: MediaQuery.of(context).copyWith(
+              textScaleFactor: 1.0,
+            ),
+            child: countryAndTextField(),
+          );
+        },
+      ),
+    );
+  }
+
+
+  countryAndTextField() {
+    return Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+           country(),
+          SizedBox( width:  marginBetweenTwoField,),
+          textFieldAndErrorMessage()
+    ] );
+  }
+
+  //--------------------------------------------------------- country
+
+  Widget country(){
+    return CountryTextFieldPicker(listener: ( country ) {
+      countryCode = country;
+      updateCallback();
+    });
+  }
+
+  //--------------------------------------------------------------- PhoneOnly
+
+  Widget textFieldAndErrorMessage(){
+    // if( widget.errorsMessageArray != null ) {
+    //   Log.i("TextFieldMobileFocus - tf_phone() -  widget.errorKeySearchingInErrorMessageArray: ${widget.errorKeySearchingInErrorMessageArray}");
+    //   Log.i("TextFieldMobileFocus - tf_phone() -  widget.errorsMessageArray: ${widget.errorsMessageArray}");
+    // }
+    return Container(
+      decoration: BoarderHelper.cardView(
+          colorLine: ColorResource.textFieldDarkBoarderLineBeforeFocused,
+          colorBackground: ColorResource.textFieldBackground,
+          radiusSize: DimensionResource.cornerTextField,
+          widthLine: 2
+      ),
+      // padding: LangApp.onlyEdgeInsets(left: 10 ),
+      alignment: LangApp.getAlignmentGeometryStartCenter(),
+      height: DimensionResource.textFieldHeight,
+      child:  Stack(children: [
+        SizedBox(
+          width: getWidthPhoneOnly(),
+          height: DimensionResource.textFieldHeight,
+        ),
+        Positioned(child:  textField(), top: Figma.h( 20) ),
+        if( ToolsValidation.isValid(  errorMessage )    ) PositionedApp.langApp(
+            child:  errorMessageWidget(),
+            left: 0,
+            top: DimensionResource.textFieldHeight - Figma.h( 17 )
+        )
+      ],),
+    );
+  }
+
+
+  Widget textField(){
+    return TextFieldApp(
+      padding: LangApp.onlyEdgeInsets(left: 10 ),
+      width: getWidthPhoneOnly()  ,
+      fontSize: Figma.h( 16 ),
+      // fontFamily: FontResource.regular,
+      maxLines: 1,
+      minLines: 1,
+      maxLength: 12,
+      hint_color: ColorResource.textFieldHint,
+      text_color: ColorResource.textFieldText,
+      hint_text: "Mobile number".tra(),
+      // autovalidateMode:  AutovalidateMode.onUserInteraction,
+      // validatorCustom:  ValidatorApp.mobile( error_text: "phone more than 7 digits".tra() ) ,
+      // errorMessage: widget.errorMessage,
+      // errorsMessageArray: widget.errorsMessageArray,
+      // errorKeySearchingInErrorMessageArray: widget.errorKeySearchingInErrorMessageArray,
+      isRemoveUnderline: true,
+      controller:   controller,
+      textInputType: TextInputType.number,
+      // decoration: getInputDecorationSwitchBetweenFocusOrUnFocus(),
+      onChanged: (text) {
+     //   setState(() => _hasFocus = text.isNotEmpty);
+
+        phone_text = text;
+        updateCallback();
+
+
+      } ,
+    );
+  }
+
+
+  getWidthPhoneOnly(){
+    return DeviceTools.getWidth(context!)
+        - CountryTextFieldPicker.widthCountryCode -  marginBetweenTwoField
+        - DimensionResource.marginLeftRightScreen
+        -  DimensionResource.marginLeftRightScreen
+        -  marginBetweenTwoField;
+  }
+
+  //---------------------------------------------------------------- call back
+
+  updateCallback(){
+     callback( countryCode??"", phone_text??"" );
+  }
+
+  //---------------------------------------------- error
+
+  errorMessageWidget(){
+    double size = Figma.h( 10);
+    // Log.i("errorMessageWidget() - errorMessage: $errorMessage");
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          Icon( Icons.info_outline, size: size, color: ColorResource.red,),
+          SizedBox( width:  5 ,),
+          TextApp(  errorMessage??"",
+            color: ColorResource.redMaterial,
+            fontSize: size,
+            // fontFamily: FontResource.regular,
+          ),
+
+        ],
+      ),
+    );
+  }
+
+
+
+}
