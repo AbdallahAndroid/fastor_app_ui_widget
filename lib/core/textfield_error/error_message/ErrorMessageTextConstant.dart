7d438ddded23bef0c5f619afd52305c1db980d89
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2024-10-29 12:33:16 +0300

    ErrorMessageTextFieldValidation

---
+++ lib/core/textfield_error/error_message/ErrorMessageTextConstant.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:fastor_app_ui_widget/core/lang/LangApp.dart';
+
+class ErrorMessageTextConstant {
+
+  static String nameEmpty = "name required".tra();
+  static String nameLength = "name at least 2 letter".tra();
+
+  static String emailEmpty =  "email required".tra();
+  static String emailInvalid = "invalid email".tra();
+
+  static String phoneRequired = "phone required".tra();
+  static String phoneInvalid = "invalid phone number".tra();
+
+  static String passwordRequired = "enter password".tra();
+  static String passwordLength = "enter password at least 8 char".tra();
+
+  static String passwordConfirmRequired = "password required".tra();
+  static String passwordConfirmLength =  "enter password at least 8 char".tra();
+  static String passwordConfirmNotMatch = "password confirm not match".tra();
+
+
+
+}
---
+++ lib/core/textfield_error/error_message/ErrorMessageTextFieldValidation.dart
@@ -0,0 +1,135 @@
+
+import 'package:fastor_app_ui_widget/core/textfield_error/ErrorInputFieldGenerator.dart';
+import 'package:fastor_app_ui_widget/core/textfield_error/ErrorInputFieldModel.dart';
+import 'package:fastor_app_ui_widget/core/textfield_error/error_message/ErrorMessageTextConstant.dart';
+import 'package:fastor_app_ui_widget/core/values/ToolsValidation.dart';
+
+class ErrorMessageTextFieldValidation {
+
+  //------------------------------------------------------------ error array
+
+  static Map<String, dynamic>? form({
+    String? name,
+    String? email,
+    String? phone,
+    String? password,
+    String? passwordConfirm
+}) {
+    var errors = ErrorInputFieldArrayModel();
+
+    /// name
+    if( name != null ) {
+      if(ToolsValidation.isEmpty( name  ) ) {
+        errors.add( "name",  ErrorMessageTextConstant.nameEmpty );
+      } else if( ToolsValidation.isName( name )  == false ) {
+        errors.add( "name",  ErrorMessageTextConstant.nameLength  );
+      }
+    }
+
+    /// email
+    if( email != null ) {
+      if(ToolsValidation.isEmpty(  email ) ) {
+        errors.add( "email",  ErrorMessageTextConstant.emailEmpty );
+      } else if( ToolsValidation.isEmail(  email  )  == false ) {
+        errors.add( "email",  ErrorMessageTextConstant.emailInvalid );
+      }
+    }
+
+    /// phone
+    if( phone != null ) {
+      if(ToolsValidation.isEmpty(  phone ) ) {
+        errors.add( "phone",  ErrorMessageTextConstant.phoneRequired );
+      } else if( ToolsValidation.isPhoneMobileValidAndEnglishLetter( phone  )  == false ) {
+        errors.add( "phone",  ErrorMessageTextConstant.phoneInvalid  );
+      }
+    }
+
+    /// validate password
+    if( password != null ) {
+      if(ToolsValidation.isEmpty(  password ) ) {
+        errors.add( "password",  ErrorMessageTextConstant.passwordRequired  );
+      } else if( ToolsValidation.isPasswordValid(  password  )  == false ) {
+        errors.add( "password",  ErrorMessageTextConstant.passwordLength );
+      }
+    }
+
+    /// validate confirm password
+    if( passwordConfirm != null ) {
+      if(ToolsValidation.isEmpty(  passwordConfirm ) ) {
+        errors.add( "password_confirm",  ErrorMessageTextConstant.passwordConfirmRequired );
+      } else if( ToolsValidation.isPasswordValid(  passwordConfirm  )  == false ) {
+        errors.add( "password_confirm",  ErrorMessageTextConstant.passwordConfirmLength  );
+      }
+
+      if(  passwordConfirm !=  password ) {
+        errors.add( "password_confirm",  ErrorMessageTextConstant.passwordConfirmNotMatch );
+      }
+    }
+    if( errors.isFoundAnyError() ) {
+      return errors.getError();
+    } else {
+      return null;
+    }
+}
+
+
+  //---------------------------------------------------------------- error single field
+
+  static Map<String, dynamic>?  validateName( String? str ) {
+    if(ToolsValidation.isEmpty( str ) ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "name", ErrorMessageTextConstant.nameEmpty);
+    } else if( ToolsValidation.isName( str  )  == false ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "name",  ErrorMessageTextConstant.nameLength );
+    } else {
+      return null ;
+    }
+  }
+
+
+  static Map<String, dynamic>?  validateEmail( String? str ) {
+    if(ToolsValidation.isEmpty( str ) ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "email",  ErrorMessageTextConstant.emailEmpty );
+    } else if( ToolsValidation.isName( str  )  == false ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "email",  ErrorMessageTextConstant.emailInvalid );
+    } else {
+      return null ;
+    }
+  }
+
+
+  static Map<String, dynamic>?  validatePhone(String? phone) {
+    if(ToolsValidation.isEmpty(  phone ) ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "phone", ErrorMessageTextConstant.phoneRequired  );
+    } else if( ToolsValidation.isPhoneMobileValidAndEnglishLetter(  phone  )  == false ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "phone",  ErrorMessageTextConstant.phoneInvalid );
+    } else {
+      return null;
+    }
+  }
+
+
+  static Map<String, dynamic>?  validatePassword(String? password) {
+    if(ToolsValidation.isEmpty(  password ) ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "password",  ErrorMessageTextConstant.passwordRequired );
+    } else if( ToolsValidation.isPhoneMobileValidAndEnglishLetter(  password  )  == false ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "password",  ErrorMessageTextConstant.passwordLength );
+    } else {
+      return null;
+    }
+  }
+
+
+  static Map<String, dynamic>?  validatePasswordConfirmation(String? password, String? passwordConfirm) {
+    if(ToolsValidation.isEmpty(  passwordConfirm ) ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "password_confirm",  ErrorMessageTextConstant.passwordConfirmRequired);
+    } else if( ToolsValidation.isPasswordValid(  passwordConfirm  )  == false ) {
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "password_confirm",  ErrorMessageTextConstant.passwordConfirmLength);
+    } else if ( passwordConfirm !=  password  ){
+      return ErrorInputFieldGenerator.generateErrorArrayMessageShapeLaravelWithOneMessage( "password_confirm",   ErrorMessageTextConstant.passwordConfirmNotMatch );
+    } else {
+      return null;
+    }
+  }
+
+
+}
