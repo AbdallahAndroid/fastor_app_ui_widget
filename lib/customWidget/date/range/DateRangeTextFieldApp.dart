7236e963470bdf37bba790a941dc3b83224b654c
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2024-11-03 13:17:10 +0200

    dateRangePluginSyncfusion

--- lib/customWidget/date/range/DateRangeTextFieldApp.dart
+++ lib/customWidget/date/range/DateRangeTextFieldApp.dart
@@ -3,7 +3,7 @@ import 'package:fastor_app_ui_widget/core/boarder/BoarderHelper.dart';
 import 'package:fastor_app_ui_widget/customWidget/emptyView/EmptyView.dart';
 import 'package:fastor_app_ui_widget/customWidget/text/TextApp.dart';
 import 'package:flutter/material.dart';
-import 'widget/DatePickerHelper.dart';
+import '../../../core/date/DatePickerHelper.dart';
 import 'widget/DateRangePickerResult.dart';
 
 typedef DateRangeTextFieldSelectCallback = Function(DateRangePickerResult? dateRange);
