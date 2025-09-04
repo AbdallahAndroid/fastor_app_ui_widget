0c4383f4914686cea9bcf771a726c44ac1a4a5f2
AbdallahAndroid <abdallah.android.app@gamil.com>
2025-09-04 14:15:13 +0300

    rename color class

--- lib/customWidget/appbar/appbar_corner_inner/container_inner_corner_appbar.dart
+++ lib/customWidget/appbar/appbar_corner_inner/container_inner_corner_appbar.dart
@@ -1,5 +1,6 @@
 import 'package:fastor_app_ui_widget/core/resource/ColorResource.dart';
 import 'package:fastor_app_ui_widget/core/utils/device/DeviceTools.dart';
+import 'package:fastor_app_ui_widget/core/utils/figma/Figma.dart';
 import 'package:fastor_app_ui_widget/customWidget/appbar/appbar_corner_inner/appbar_corner_bottom_inner_utils.dart';
 import 'package:flutter/material.dart';
 
