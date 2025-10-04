a11a94c6a312a1f2f03698fb25e90a96b08da308
AbdallahAndroid <abdallah.android.app@gamil.com>
2025-10-05 00:39:08 +0300

    web responsive classes

---
+++ lib/web/responsive_views/helper/ResponsiveConstant.dart
@@ -0,0 +1,6 @@
+class ResponsiveConstant {
+
+  static double widthMobileNormal = 500;
+  static double widthChangeInCaseLandscape = 600;
+  static double heightChangeInCaseLandscape = 900;
+}
---
+++ lib/web/responsive_views/helper/ResponsiveRectangle.dart
@@ -0,0 +1,53 @@
+
+import 'package:fastor_app_ui_widget/core/utils/device/DeviceTools.dart';
+import 'package:fastor_app_ui_widget/customWidget/listview/ListViewApp.dart';
+import 'package:fastor_app_ui_widget/web/responsive_views/helper/ResponsiveConstant.dart';
+import 'package:flutter/material.dart';
+/**
+ Here the defaule page type is "portrait", what to do when responsive to landscape
+ */
+class ResponsiveToLandscape {
+
+
+
+  static double getDeviceWidthInCaseLandscape(BuildContext context){
+    if( DeviceTools.isLandscape(context) ) {
+      return ResponsiveConstant.widthChangeInCaseLandscape;
+    }
+    return DeviceTools.getWidth( context );
+  }
+
+
+  static double getDeviceHeightInCaseLandscape(BuildContext context){
+    if( DeviceTools.isLandscape(context) ) {
+      return ResponsiveConstant.heightChangeInCaseLandscape;
+    }
+    return DeviceTools.getHeight( context );
+  }
+
+
+  /**
+      - rule:
+      +portrait:
+      child1,
+      child2
+      +landscape:
+      child1, child2
+
+   - pages:
+      "PaginateWidget" in page type "Portrait" make the direction of axis is vertical,
+      while in page type is "Landscape"  make the direction of axis is vertical.
+
+   */
+  static portraitVertical_to_landscapeHorizontal(BuildContext context, {
+    List<Widget> children = const <Widget>[]
+  }) {
+      if( DeviceTools.isPortrait( context) ) {
+        return ListViewApp.t(context: context, children: children, axis: Axis.vertical);
+      } else {
+        return ListViewApp.t(context: context, children: children, axis: Axis.horizontal);
+      }
+  }
+
+
+}
