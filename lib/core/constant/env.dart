837e7962f0bb4c1d95687839fe7fd1dd7707238d
AbdallahAndroid <abdallah.android.app@gamil.com>
2025-11-01 20:15:47 +0200

    offline

--- lib/core/constant/env.dart
+++ lib/core/constant/env.dart
@@ -1,10 +1,11 @@
 var isTestEnvironment = false ;
 var isLiveEnvironment = ! isTestEnvironment;
 
-int databaseOfflineVersion = 1;
+
 
 class env {
   static String countryCodeDefault = "+966";
 
   static var isTest = isTestEnvironment;
+  static var databaseOfflineVersion = 1;
 }
